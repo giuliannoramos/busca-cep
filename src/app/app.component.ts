bdd83e02a2f5be196a32d66bcf53d7ea2ce7b42e
Giulianno Ramos <giuleramos@gmail.com>
2024-03-30 16:23:38 -0300

    refactor: Update file vercel json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormPageComponent } from './modules/pages/form-page/form-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-  <router-outlet />`
+  template: ` <router-outlet />`,
 })
 export class AppComponent {
   title = 'BuscarCep';
